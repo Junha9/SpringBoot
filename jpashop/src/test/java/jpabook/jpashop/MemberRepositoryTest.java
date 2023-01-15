183aaa46cdb12e6a9a672797841a3d7f3992985f
Junha9 <ghno20@gmail.com>
2023-01-15 17:04:33 +0900

    0115 complete setting

--- jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -5,17 +5,21 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class MemberRepositoryTest {
+class MemberRepositoryTest {
 
     @Autowired MemberRepository memberRepository;
 
     @Test
-    public void testMember() throws Exception {
+    @Transactional
+    @Rollback(false)
+    public void testMember() throws Exception{
         Member member = new Member();
         member.setUsername("memberA");
 
@@ -24,6 +28,7 @@ public class MemberRepositoryTest {
 
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        Assertions.assertThat(findMember).isEqualTo(member);
 
     }
 }
